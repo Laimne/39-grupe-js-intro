2bb36d61f1599f43f43d8c2913096bbf295a2174
Laimne <107036125+Laimne@users.noreply.github.com>
2022-06-30 11:36:58 +0300

    change

---
+++ js/strings.js
@@ -0,0 +1,14 @@
+
+
+
+const name = "Petras";
+console.log(name);
+
+const lastname = "Petraitis";
+console.log(lastname);
+
+const sentense = "Labas rytas Lietuva!";
+console.log(sentense);
+
+const p = "Labas rytas,Labas rytas,Labas rytas";
+console.log(p);
