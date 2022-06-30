bc5f35dd51aa5d30e9d0056c0590c6dc4c98d156
Laimne <107036125+Laimne@users.noreply.github.com>
2022-06-30 13:42:20 +0300

    array

---
+++ js/array.js
@@ -0,0 +1,19 @@
+
+
+const marks = [10,2,8,4,6];
+console.log(marks);
+
+const pirmas = marks[0];
+const antras = marks[1];
+const trecias = marks[2];
+const ketvirtas = marks[3];
+
+
+
+const suma = pirmas+antras+trecias+ketvirtas;
+
+
+
+
+
+
