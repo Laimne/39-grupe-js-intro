96f7280d50fee388a6150daea329b52ac1e4241f
Laimne <107036125+Laimne@users.noreply.github.com>
2022-06-30 14:01:53 +0300

    change

---
+++ js/for.js
@@ -0,0 +1,17 @@
+
+
+const marks =[10,2,8,4,6];
+
+let sums=0;
+let index=0;
+
+for(let i=0; i<marks.length;i++){
+suma +=marks[index++];
+}
+
+
+const vidurkis=suma/marks.length;
+
+console.log("suma:",suma);
+console.log("vidurkis:",vidurkis);
+
