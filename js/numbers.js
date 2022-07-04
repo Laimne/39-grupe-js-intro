deb94d1e275fdec2731827d175b554cd3116368c
Laimne <107036125+Laimne@users.noreply.github.com>
2022-07-04 11:57:04 +0300

    change

--- js/numbers.js
+++ js/numbers.js
@@ -1,4 +1,7 @@
-console.clear();
+
+
+
+
 /*
 
 
@@ -12,6 +15,59 @@ NUMBERS
 const metai=2022;
 console.log(2022);
 console. log(metai);
+
+
+const a=7;
+const b=5;
+const suma = a+b;
+console.log(a,b,suma);
+console.clear();
+
+let total=1000;
+console.log(total);
+
+total=total*2;
+console.log(total);
+
+total=total*2;
+console.log(total);
+
+let silense=0;
+console.log(silense);
+
+
+silense=silense +5;
+console.log(silense);
+
+silense=silense +5;
+console.log(silense);
+
+silense-=2;
+silense/=2;
+silense*=2;
+console.log(silense);
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
