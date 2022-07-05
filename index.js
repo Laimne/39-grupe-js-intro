ec6831b4191b12018ad16ffbd1884e7c2c6230e9
Laimne <107036125+Laimne@users.noreply.github.com>
2022-07-05 09:58:26 +0300

    tusciaFunkcija

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// single line coment
+// single line comment
+
+/*
+    multi
+    line
+    comment
+*/
+
+console.log(1);     // comment on the side
+console.log(2);
+
+// above code
+console.log(3);
+
+// another comment
+console.log(4);
