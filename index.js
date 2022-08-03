5e7b35dda160de7b7550bde3f234e62d5c6a90ba
Laimne <107036125+Laimne@users.noreply.github.com>
2022-08-03 17:12:09 +0300

    change

--- index.js
+++ index.js
@@ -14,3 +14,11 @@ console.log(3);
 
 // another comment
 console.log(4);
+
+const array1 = [5, 12, 8, 130, 44];
+
+const found = array1.find(element => element > 10);
+
+console.log(found);
+// expected output: 12
+
